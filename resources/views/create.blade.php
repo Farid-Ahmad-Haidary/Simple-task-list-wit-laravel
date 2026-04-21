1b897cba07d1d643c92e9263bef42c8995e9fa98
Farid Ahmad Haidary <faridahmadhaidary01@gmail.com>
2026-04-21 16:11:16 +0430

    Changes are concealed

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('title', 'Add Task')
+@section('styles')
+    <style>
+        .error_message {
+            color: red;
+            font-size: 14px;
+        }
+    </style>
+@endsection
+
+
 
 @section('content')
     <div class="max-w-md mx-auto mt-10 bg-white p-6 rounded-lg shadow-md">
@@ -10,21 +20,33 @@
             <div class="mb-4">
                 <label for="title" class="block text-sm font-medium text-gray-700 mb-2">Title</label>
                 <input type="text" name="title" id="title"
-                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    required>
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                @error('title')
+                    <p class="error_message">{{ $message }}</p>
+                @enderror
             </div>
+
+
             <div class="mb-4">
                 <label for="description" class="block text-sm font-medium text-gray-700 mb-2">Description</label>
                 <textarea name="description" id="description" rows="5"
-                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    required></textarea>
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+                @error('description')
+                    <p class="error_message">{{ $message }}</p>
+                @enderror
             </div>
+
+
             <div class="mb-4">
                 <label for="long_description" class="block text-sm font-medium text-gray-700 mb-2">Long Description</label>
                 <textarea name="long_description" id="long_description" rows="10"
-                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    required></textarea>
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+                @error('long_description')
+                    <p class="error_message">{{ $message }}</p>
+                @enderror
             </div>
+
+
             <div class="text-center">
                 <button type="submit"
                     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500">Add
